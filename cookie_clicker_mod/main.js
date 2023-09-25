ff7eaa503d3296d46437f8056e18025aa37fbba1
Samstercraft <samstercraft77@gmail.com>
2023-09-24 18:46:29 -0700

    cleaned up cc mod indents

--- cookie_clicker_mod/main.js
+++ cookie_clicker_mod/main.js
@@ -1,31 +1,30 @@
 if (typeof CCSE === 'undefined') {
     Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
-  }
-  
-  if (typeof CraftClicker === 'undefined') {
+}
+
+if (typeof CraftClicker === 'undefined') {
     var CraftClicker = {};
-  }
-  
-  CraftClicker.name = 'Craft Clicker';
-  CraftClicker.version = '1.0';
-  CraftClicker.GameVersion = '2.052';
-  
-  CraftClicker.launch = function () {
+}
+
+CraftClicker.name = 'Craft Clicker';
+CraftClicker.version = '1.0';
+CraftClicker.GameVersion = '2.052';
+
+CraftClicker.launch = function () {
     if (CCSE.ConfirmGameVersion(CraftClicker.name, CraftClicker.version, CraftClicker.GameVersion)) {
 
-      // code to run on load
-
+        CraftClicker.isLoaded = true;
     }
-  };
+};
 
   // define functions
-  
-  if (!CraftClicker.isLoaded) {
+
+if (!CraftClicker.isLoaded) {
     if (CCSE && CCSE.isLoaded) {
-      CraftClicker.launch();
+        CraftClicker.launch();
     } else {
-      if (!CCSE) var CCSE = {};
-      if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
-      CCSE.postLoadHooks.push(CraftClicker.launch);
+        if (!CCSE) var CCSE = {};
+        if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+        CCSE.postLoadHooks.push(CraftClicker.launch);
     }
-  }
+}
