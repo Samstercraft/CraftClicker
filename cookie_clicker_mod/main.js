47a591c139c69697683dc8a3a1d944ec171e35d0
Samstercraft <samstercraft77@gmail.com>
2023-09-24 18:42:35 -0700

    cookie clicker base mod

---
+++ cookie_clicker_mod/main.js
@@ -0,0 +1,31 @@
+if (typeof CCSE === 'undefined') {
+    Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (0 ? 'Beta/' : '') + 'CCSE.js');
+  }
+  
+  if (typeof CraftClicker === 'undefined') {
+    var CraftClicker = {};
+  }
+  
+  CraftClicker.name = 'Craft Clicker';
+  CraftClicker.version = '1.0';
+  CraftClicker.GameVersion = '2.052';
+  
+  CraftClicker.launch = function () {
+    if (CCSE.ConfirmGameVersion(CraftClicker.name, CraftClicker.version, CraftClicker.GameVersion)) {
+
+      // code to run on load
+
+    }
+  };
+
+  // define functions
+  
+  if (!CraftClicker.isLoaded) {
+    if (CCSE && CCSE.isLoaded) {
+      CraftClicker.launch();
+    } else {
+      if (!CCSE) var CCSE = {};
+      if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+      CCSE.postLoadHooks.push(CraftClicker.launch);
+    }
+  }
